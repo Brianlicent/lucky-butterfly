6becacecb8716aa63888f9a272867acbea232f75
Buildify <buildify-bot@buildify.dev>
2026-09-06 20:50:11 +0000

    Updated project files

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -152,7 +152,7 @@ export default function AviatorGame() {
               top: `${100 - multiplier * 12}%`,
             }}
           >
-            <div className="text-5xl butterfly-flap">🦋</div>
+            <div className="text-9xl butterfly-flap">🦋</div>
           </div>
 
           {/* Multiplier Display */}
